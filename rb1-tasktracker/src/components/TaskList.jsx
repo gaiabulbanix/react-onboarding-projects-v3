ae47155bd6e7e7db452f501152d2655b20885d1d
gaiabulbanix <frpcastillo@gmail.com>
2025-11-13 20:30:50 +0800

    feat: finished delete functionality

--- rb1-tasktracker/src/components/TaskList.jsx
+++ rb1-tasktracker/src/components/TaskList.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 import TaskItem from "./TaskItem";
 
-function TaskList({ onGenerateTaskArray = [] }) {
-
-    // delete task from storage array and task list (item)
-    function handleDelTask(task) {
-        console.log(task);
-    }
+function TaskList({ onGenerateTaskArray = [], onDelTask }) {
 
     // jsx block
     return (
         <section className="task-list">
             <h2>Task List</h2>
             <ul>
+                {/* make a list using a map of the array - if there is no valid array, say "no tasks yet", if there is one, make the list*/}
                 {onGenerateTaskArray.length === 0 ? (
                     <li>No tasks yet</li>
                 ) : (
+                    // remember that array and filter uses (element, index, array) => callback function
                     onGenerateTaskArray.map((task, index) => (
                         <TaskItem
                             key={index}
                             task={task}
-                            onDelTask={handleDelTask}
+                            onDelTask={() => onDelTask(index)}
                         />
                     ))
                 )}
