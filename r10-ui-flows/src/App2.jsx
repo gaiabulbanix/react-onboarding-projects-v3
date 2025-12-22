20df9c8dc747139c15b9ec521079c41527703368
gaiabulbanix <frpcastillo@gmail.com>
2025-12-22 19:41:16 +0800

    feaT:added another action row for enabling/disabling notifications on a toggle

--- r10-ui-flows/src/App2.jsx
+++ r10-ui-flows/src/App2.jsx
@@ -7,6 +7,7 @@ import FormInput from './components/FormInput';
 export default function App2() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+    const [notifications, setNotifications] = useState(false);
 
     return (
         <div className="min-h-screen bg-gray-100 p-6">
@@ -27,7 +28,7 @@ export default function App2() {
                         `}
                             disabled={name === '' || email === ''}
                         >
-                            Button
+                            Submit
                         </button>
                     }
                 >
@@ -38,6 +39,27 @@ export default function App2() {
                         placeholder="Enter email here."
                         onChange={(e) => setEmail(e.target.value)}
                     />}>
+                </ActionRow>
+                <ActionRow
+                    left={<p>Enable Notifications</p>}
+                    right={
+                        <button
+                            className={`py-3 px-1.5 rounded-md
+                                ${notifications
+                                    ? "bg-blue-600 text-white"
+                                    : "bg-blue-300"
+                                }
+                                `}
+                            onClick={() => setNotifications(prev => !prev)}
+                        >
+                            {notifications
+                                ? "Enabled"
+                                : "Disabled"
+                            }
+                        </button>
+                    }
+
+                >
                 </ActionRow>
             </Panel>
         </div>
