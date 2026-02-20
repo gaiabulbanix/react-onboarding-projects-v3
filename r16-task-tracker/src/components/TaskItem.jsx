9ccbbdbdb980e5eaa6639db2435e9c63ef885340
gaiabulbanix <frpcastillo@gmail.com>
2026-02-20 14:33:34 +0800

    feat:extracted TaskList into TaskList.jsx component, completed wiring

--- r16-task-tracker/src/components/TaskItem.jsx
+++ r16-task-tracker/src/components/TaskItem.jsx
@@ -1,5 +1,22 @@
-export default function TaskItem({ className = '', children, }) {
-    <li>
-        {children}
-    </li>
+import Button from "./Button";
+
+export default function TaskItem({ className = '', task, onToggle, onDelete }) {
+    return (
+        <li
+            className={`${className}`}
+        >
+            ID: {task.id} - {task.title} - {task.completed ? 'Complete!' : 'Pending'}
+            <input
+                type="checkbox"
+                checked={task.completed}
+                onChange={() => onToggle(task.id)}
+            />
+            <Button
+                className="px-1 py-0.5"
+                onClick={() => onDelete(task.id)}
+            >
+                Delete Task
+            </Button>
+        </li>
+    );
 }
