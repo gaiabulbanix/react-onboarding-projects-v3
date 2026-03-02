c06dbb9284bef881b3c8b615a677dd948c20a2ee
gaiabulbanix <frpcastillo@gmail.com>
2026-03-02 22:20:27 +0800

    feat: made tasks and input states and wired them to demo elements

--- r16-task-tracker/src/rebuild/TaskTracker.jsx
+++ r16-task-tracker/src/rebuild/TaskTracker.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+
 export default function TaskTracker() {
+    // states
+    const [tasks, setTasks] = useState([]);
+    const [input, setInput] = useState('');
+
     return (
         <>
             <div>
                 <h1>Task Tracker</h1>
+                <p>Total Tasks: {tasks.length}</p>
             </div>
             <div>
-                <form action="">
-
+                <form>
+                    <input
+                        type="text"
+                        value={input}
+                    />
                 </form>
             </div>
             <div>
