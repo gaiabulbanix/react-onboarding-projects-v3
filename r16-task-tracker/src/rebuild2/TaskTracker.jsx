6b6821724e2bc1e8e21451376dfc1a4ba51285af
gaiabulbanix <frpcastillo@gmail.com>
2026-03-09 23:29:49 +0800

    feat: created basic starter skeleton for rebuild 2

--- r16-task-tracker/src/rebuild2/TaskTracker.jsx
+++ r16-task-tracker/src/rebuild2/TaskTracker.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
 export default function TaskTracker() {
+    // states
+    const [taskArray, setTaskArray] = useState([])
+    const [taskItem, setTaskItem] = useState('');
+
+    // handlers
+    const handleAddTask = () => {
+        return
+    };
+
+    // jsx block
     return (
-        <p>hello world</p>
+        <>
+            <div>
+                <form>
+                    <input
+                        type="text"
+                    />
+                    <button
+                        type="submit"
+                    >
+                        Add Task
+                    </button>
+                </form>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                    </li>
+                </ul>
+            </div>
+        </>
     );
 }
