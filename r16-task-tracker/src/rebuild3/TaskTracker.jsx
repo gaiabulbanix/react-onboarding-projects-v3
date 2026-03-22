d4d9aad7dc8ac423cc174eade47f1beeed82397d
gaiabulbanix <frpcastillo@gmail.com>
2026-03-22 23:07:17 +0800

    feat: created handler skeletons for adding and displaying tasks, created form, text input, and submit button skeleton and props

--- r16-task-tracker/src/rebuild3/TaskTracker.jsx
+++ r16-task-tracker/src/rebuild3/TaskTracker.jsx
@@ -5,8 +5,40 @@ export default function TaskTracker() {
     const [taskList, setTaskList] = useState([]);
     const [taskItem, setTaskItem] = useState('');
 
+    // handler
+
+    const handleAddTask = () => {
+        setTaskList(prev => {
+            taskList.map(task => )
+        });
+    };
+
+    const handleDisplayTaskList = () => {
+        setTaskList
+    };
+
+    const handleDeleteTaskItem;
+
+    const handleDeleteAllTasks;
+
     // jsx
     return (
-        ''
+        <>
+            <div>
+                <form
+                    onSubmit={() => handleAddTask}
+                >
+                    <input
+                        type="text"
+                        value={(e) => setTaskItem(e.target.value)}
+                    />
+                    <button
+                        type="Submit"
+                    >
+                        Add Task
+                    </button>
+                </form>
+            </div>
+        </>
     );
 }
