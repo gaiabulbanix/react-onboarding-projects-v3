13301a7598b4ea1bce84430fe4e14eb95e3285bb
gaiabulbanix <frpcastillo@gmail.com>
2026-04-02 20:11:23 +0800

    feat: created initial task list filter logic

--- r16-task-tracker/src/components/TaskTracker.jsx
+++ r16-task-tracker/src/components/TaskTracker.jsx
@@ -50,6 +50,18 @@ export default function TaskTracker() {
         setTaskList([]);
     }
 
+    // filter logic
+    const filteredTasks = () => {
+        if (filter === 'all') {
+            // return tasklist here regardless of completion
+        } else if (filter === 'completed') {
+            // return tasklist but filtered only for completed === true, use taskList.filter probably
+        } else if (filter === 'pending') {
+            // return tasklist but filtered only for completed === false, use taskList.filter probably
+        } else return;
+        // return if there's an error
+    };
+
     // jsx block
     return (
         <>
