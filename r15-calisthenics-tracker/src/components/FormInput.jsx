69e101c8864be806722f134069680d1aa51262e9
gaiabulbanix <frpcastillo@gmail.com>
2026-05-18 23:22:02 +0800

    fix: added UI panel styling to align with r16, minor handler name edits, started edit mode structure for WorkoutItem

--- r15-calisthenics-tracker/src/components/FormInput.jsx
+++ r15-calisthenics-tracker/src/components/FormInput.jsx
@@ -1,4 +1,4 @@
-export default function FormInput({ inputClassName = '', wrapperClassName = '', label, htmlFor, ...inputProps }) {
+export default function FormInput({ inputClassName = '', wrapperClassName = '', label, htmlFor, ...inputProps, }) {
     return (
         <div className={`flex flex-col gap-2 ${wrapperClassName}`}>
             {label && <label htmlFor={htmlFor}>{label}</label>}
--- r15-calisthenics-tracker/src/components/WorkoutItem.jsx
+++ r15-calisthenics-tracker/src/components/WorkoutItem.jsx
@@ -1,27 +1,56 @@
 import Panel from "./Panel";
 import Button from "./Button";
 
-export default function WorkoutItem({ className = '', index, workoutItem, handleRemoveWorkout, handleEditWorkout }) {
+export default function WorkoutItem({ className = '', index, workoutItem, handleRemoveWorkout, handleEditWorkout, }) {
+    const [editState, setEditState] = useState(false);
+    const [editWorkoutInput, setEditWorkoutInput] = useState('');
+    const [editWorkoutReps, setEditWorkoutReps] = useState('');
+
     return (
-        <li
-            className="flex gap-4 items-center justify-between">
-            <span>
-                {index + 1} - {workoutItem.workout} - {workoutItem.reps} rep(s)
-            </span>
-            <div className="flex gap-2">
-                <Button
-                    buttonStyle="danger"
-                    buttonSize="sm"
-                    onClick={() => handleRemoveWorkout(workoutItem.id)}>
-                    Remove
-                </Button>
-                <Button
-                    buttonStyle="primary"
-                    buttonSize="sm"
-                    onClick={() => handleEditWorkout(workoutItem)}>
-                    Edit
-                </Button>
-            </div>
-        </li>
+        editState
+            ?
+            <li>
+                <div>
+                    <input
+                        type="text"
+                        value={editWorkoutInput}
+                        onChange={(e) => setEditWorkoutInput(e.target.value)}
+                        onKeyDown={(e) => {
+                            if (e.key === enter) {
+                                e.preventDefault();
+                                handleEditWorkout()
+                            }
+                        }}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="number"
+                        value={editWorkoutReps}
+                        onChange={(e) => setEditWorkoutReps(e.target.value)}
+                    />
+                </div>
+            </li>
+            :
+            <li
+                className="flex gap-4 items-center justify-between">
+                <span>
+                    {index + 1} - {workoutItem.workout} - {workoutItem.reps} rep(s)
+                </span>
+                <div className="flex gap-2">
+                    <Button
+                        buttonStyle="danger"
+                        buttonSize="sm"
+                        onClick={() => handleRemoveWorkout(workoutItem.id)}>
+                        Remove
+                    </Button>
+                    <Button
+                        buttonStyle="primary"
+                        buttonSize="sm"
+                        onClick={() => handleEditWorkout(workoutItem)}>
+                        Edit
+                    </Button>
+                </div>
+            </li>
     );
 }
