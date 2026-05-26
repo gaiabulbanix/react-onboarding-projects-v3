e75331be6df48860fed4cf1fdfcb4b3c7c48e88c
gaiabulbanix <frpcastillo@gmail.com>
2026-05-26 23:06:08 +0800

    fix: corrected positioning of inputs and button to adjust for spacing in r15

--- r15-calisthenics-tracker/src/components/CalisthenicsTracker.jsx
+++ r15-calisthenics-tracker/src/components/CalisthenicsTracker.jsx
@@ -93,7 +93,7 @@ export default function CalisthenicsTracker() {
 
             <Panel className="mt-6 w-full max-w-3xl">
                 <WorkoutInput
-                    className="flex flex-col"
+                    className="flex justify-between items-end"
                     workout={workout}
                     setWorkout={setWorkout}
                     workoutReps={workoutReps}
