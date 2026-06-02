76f9ece3bc66406bdf7eb8e59ce6fe3af6f5a132
gaiabulbanix <frpcastillo@gmail.com>
2026-06-02 23:20:11 +0800

    fix: removed unused prop in TaskList extraction

--- r16-task-tracker/src/components/TaskTracker.jsx
+++ r16-task-tracker/src/components/TaskTracker.jsx
@@ -138,7 +138,6 @@ export default function TaskTracker() {
 
                 <TaskList
                     tasks={filteredTasks}
-                    taskList={taskList}
                     handleToggleTask={handleToggleTask}
                     handleRemoveTask={handleRemoveTask}
                     handleEditTask={handleEditTask}
